c169a045fd11693cfef8eb34e6a3547939aa5e17
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2025-07-09 10:14:02 +0700

    Update footer.php

--- application/views_old/template/festavalive/footer.php
+++ application/views_old/template/festavalive/footer.php
@@ -3,9 +3,9 @@
             <div class="container">
                 <div class="row">
 
-                    <div class="col-lg-6 col-12">
+                    <!-- <div class="col-lg-6 col-12">
                         <h2 class="text-white mb-lg-0">El shaddai Church</h2>
-                    </div>
+                    </div> -->
 
                     <div class="col-lg-6 col-12 d-flex justify-content-lg-end align-items-center">
                         <ul class="social-icon d-flex justify-content-lg-end">
