b10de6f3b0f3ebc12b0c193ae9535265dfc9b626
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2025-08-27 14:12:16 +0700

    Update registrasiModal.php

--- application/views/registrasiModal.php
+++ application/views/registrasiModal.php
@@ -150,7 +150,8 @@ Registrasi Modal Baru
                     <div class="col-12 mt-5 divsudahpernahfondationclass">
                       <div class="row">
                         <div class="col-12">
-                          <h5 class="">Sudah Pernah Membuat Kartu Anggota Jemaat ESC?</h5>
+                          <!-- <h5 class="text">Sudah Pernah Membuat Kartu Anggota Jemaat ESC?</h5> -->
+                          <h3 class="text-center">Sudah Pernah Membuat Kartu Anggota Jemaat ESC?</h3>
 
                         </div>
                         <div class="col-12">
