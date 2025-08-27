c7d7c997ac031554fabd6e448ea3eff3e64c9957
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2025-08-27 10:34:07 +0700

    Update registrasiModal.php

--- application/views/registrasiModal.php
+++ application/views/registrasiModal.php
@@ -78,10 +78,18 @@
       }
   }
 
-  .nav.nav-progress .nav-link,
+  /* .nav.nav-progress .nav-link,
   .nav.nav-progress .nav-link .num {
       color: #ff5008 !important;
+  } */
+
+
+  .nav.nav-progress .nav-link .num {
+      background-color: #ff5008 !important;
+      border-color: #ff5008 !important; /* kalau ada border */
+      color: #fff !important; /* teks di dalam bulatan biar kontras */
   }
+
 
 
 </style>
