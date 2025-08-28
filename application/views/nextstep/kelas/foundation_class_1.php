2bdcddd13636707aaa2e43318367188cd47292c4
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2025-08-28 10:10:32 +0700

    update background fc1

--- application/views/nextstep/kelas/foundation_class_1.php
+++ application/views/nextstep/kelas/foundation_class_1.php
@@ -338,7 +338,7 @@ $this->load->view('template/festavalive/header'); ?>
       body {
       margin: 0;
       padding: 0;
-      background-color: #e9d6a8;
+      background: linear-gradient(63deg, #fffaf5, #ffb347);
       font-family: 'Figtree', sans-serif;
       color: #111;
       line-height: 1.7;
