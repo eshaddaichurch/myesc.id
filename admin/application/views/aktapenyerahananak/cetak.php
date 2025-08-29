fe14acc52323b818557d494f042e5d72392d1615
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2025-08-29 10:56:14 +0700

    Update cetak.php

--- admin/application/views/aktapenyerahananak/cetak.php
+++ admin/application/views/aktapenyerahananak/cetak.php
@@ -194,7 +194,7 @@ $pdf->SetXY(117, 248);
 $pdf->writeHTML($html, true, false, true, false, '');
 
 // $html = $css . '<span class="default-text text-bold">' . GEMBALAGEREJA . '</span>';
-$html = $css . '<span class="default-text dilakukan-oleh">' . $rsakta->dilakukanoleh . '</span>';
+$html = $css . '<span class="default-text text-bold">' . $rsakta->dilakukanoleh . '</span>';
 $pdf->SetXY(121, 274);
 $pdf->writeHTML($html, true, false, true, false, '');
 
