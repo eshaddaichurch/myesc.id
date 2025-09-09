b30c25346cf45c736244979cdba34da988d23c0e
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2025-09-09 11:28:26 +0700

    perubahan warna pada card

--- application/views/baptisan/formpermohonanbaptisan.php
+++ application/views/baptisan/formpermohonanbaptisan.php
@@ -4,7 +4,8 @@
 
   body {
     font-family: 'Figtree', sans-serif;
-    background-color: #e8d5a7;
+    /* background-color: #e8d5a7; */
+    background-color: linear-gradient(63deg, #fffaf5, #ffb347);
     padding-top: 60px;
     margin: 0;
   }
