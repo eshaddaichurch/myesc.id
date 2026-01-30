cf72eaedfed8e332a6001b469e5c7cb2bfc61c76
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-01-30 21:47:04 +0700

    Update Login_model.php

--- community/application/models/Login_model.php
+++ community/application/models/Login_model.php
@@ -4,11 +4,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Login_model extends CI_Model
 {
 
+    // public function cek_login($username, $password)
+    // {
+    //     $query = "select * from v_dcmember where username='" . $username . "' and password='" . $password . "'";
+    //     return $this->db->query($query);
+    // }
+
     public function cek_login($username, $password)
     {
-        $query = "select * from v_dcmember where username='" . $username . "' and password='" . $password . "'";
-        return $this->db->query($query);
+        return $this->db
+            ->where('LOWER(TRIM(username))', strtolower(trim($username)))
+            ->where('password', $password)
+            ->where('statusaktif', 'Aktif')
+            ->get('v_dcmember');
     }
+
 }
 
 /* End of file Login_model.php */
