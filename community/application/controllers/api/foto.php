f5909bc1f931b6ee03a2987814f07d354cfce426
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-02-02 22:02:01 +0700

    Update foto.php

--- community/application/controllers/api/foto.php
+++ community/application/controllers/api/foto.php
@@ -3,9 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Foto extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        // CORS headers untuk React Native
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            exit(0);
+        }
+    }
+
     public function jemaat($filename = null)
     {
-        // naik 1 level dari community ke myesc.id
+        // Naik 1 level dari community ke myesc.id
         $basePath = realpath(FCPATH . '../admin/uploads/jemaat/') . DIRECTORY_SEPARATOR;
         $noFoto   = realpath(FCPATH . '../images/nofoto.png');
 
@@ -13,9 +25,8 @@ class Foto extends CI_Controller {
             return $this->outputImage($noFoto);
         }
 
-        // security: cegah ../
+        // Security: cegah path traversal
         $filename = basename($filename);
-
         $filePath = $basePath . $filename;
 
         if (!file_exists($filePath) || !is_file($filePath)) {
