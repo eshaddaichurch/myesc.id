118b94ee1b716d3dcd4f9c1217dac2e0c806fc72
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-02-03 22:46:18 +0700

    all back

--- community/application/models/api/Konfirmasidc_api_model.php
+++ community/application/models/api/Konfirmasidc_api_model.php
@@ -1,14 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Konfirmasidc_api_model extends CI_Model {
-
-    public function getDetail($idpermohonan)
+class Konfirmasidc_api_model extends CI_Model
+{
+    public function getPermohonanByDc($iddc)
     {
         return $this->db
-            ->where('idpermohonan', $idpermohonan)
+            ->where('iddc', $iddc)
+            ->order_by('tglpermohonan', 'DESC')
             ->get('v_dcmember_permohonan')
-            ->row();
+            ->result_array();
     }
-
 }
