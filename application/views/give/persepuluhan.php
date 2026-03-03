eb7d43f7e6d5860dbbac558a6c462f8cd9682f27
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-03-03 13:53:42 +0700

    Update persepuluhan.php

--- application/views/give/persepuluhan.php
+++ application/views/give/persepuluhan.php
@@ -15,33 +15,6 @@ body {
 }
 
 /* ================= SECTION ================= */
-.generosity-hero {
-  position: relative;
-  min-height: 100svh; /* paling aman untuk mobile modern */
-  width: 100%;
-  background: url("<?php echo base_url('myesc.id/assets/gambar/giving.jpg'); ?>") center center no-repeat;
-  background-size: cover;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  color: #fff;
-  padding: 80px 20px;
-}
-
-.generosity-hero {
-  min-height: 100vh;
-  min-height: 100svh;
-}
-
-
-
-.generosity-hero::before {
-  content: "";
-  position: absolute;
-  inset: 0;
-  background: rgba(0,0,0,0.65);
-}
 
 .giving-wrapper {
   padding: 120px 20px 80px;
@@ -264,9 +237,7 @@ body {
 
 <?php $this->load->view('template/festavalive/topmenu'); ?>
 
-
 <section class="giving-wrapper">
-<div class="generosity-hero">
   <div class="giving-container">
 
     <h2 class="section-title">Persembahan Persepuluhan</h2>
@@ -338,7 +309,6 @@ body {
     </div>
 
   </div>
-</div>
 </section>
 
 <script>
