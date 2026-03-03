86fc0bf87d7aeb7a161f89dcae8c8797e8dfa717
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-03-03 11:51:09 +0700

    Update Give.php

--- application/controllers/Give.php
+++ application/controllers/Give.php
@@ -7,14 +7,20 @@ class Give extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('Home_model');
+		// $this->load->model('Ourlocation_model');
 	}
 
-	// halaman utama GIVE
-	public function index()
+	public function index($idmenu = null)
 	{
-		$data['title'] = 'GIVE';
-		$data["rowinfogereja"] = $this->Home_model->get_infogereja();
-		$this->load->view('give/giving', $data);
+		$data['title']= 'GIVE';
+		if ($idmenu !== null) {
+			$idmenu = $this->encrypt->decode($idmenu);
+			$data['menu'] = $idmenu;
+		} else {
+			$data['menu'] = '';
+		}
+		$data["rowinfogereja"] = $this->Home_model->get_infogereja();	
+		$this->load->view('give/giving',$data);
 	}
 
 	// halaman detail dinamis
@@ -35,4 +41,7 @@ class Give extends CI_Controller {
 			show_404();
 		}
 	}
+	
+
 }
+
