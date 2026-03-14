b5a7e3fab8ab266c7787cafdc181174bcfb35231
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-03-14 22:52:11 +0700

    update API

--- community/application/controllers/api/Home.php
+++ community/application/controllers/api/Home.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-
+class Home extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
@@ -34,15 +34,25 @@ class Home extends CI_Controller {
             AND statuskeanggotaan = 'Anggota'
         ")->row()->jumlah;
 
+        // Ambil data user berdasarkan iddc (DM nya)
+        $user = $this->db->query("
+        SELECT j.foto, j.namalengkap
+        FROM v_dcmember v
+        JOIN jemaat j ON j.idjemaat = v.idjemaat
+        WHERE v.iddc = '$iddc'
+        AND v.statuskeanggotaan = 'Disciples maker'
+        LIMIT 1
+        ")->row();
+
         echo json_encode([
             'status' => true,
             'data' => [
-                'dm'     => (int)$dm,
-                'core'   => (int)$core,
-                'member' => (int)$member,
-                'total'  => (int)($core + $member) // ✅ FIX
+                'dm' => (int) $dm,
+                'core' => (int) $core,
+                'member' => (int) $member,
+                'total' => (int) ($core + $member),
+                'foto' => 'https://myesc.id/myesc.id/admin/uploads/jemaat/' . $user->foto,
             ]
         ]);
     }
-
 }
