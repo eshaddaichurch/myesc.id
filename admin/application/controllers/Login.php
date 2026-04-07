5f27710ddca6e8a47dd637d17c73a2101f831ccd
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-04-07 14:43:58 +0700

    fix bug login

--- admin/application/controllers/Login.php
+++ admin/application/controllers/Login.php
@@ -1,29 +1,28 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
-
+class Login extends CI_Controller
+{
     public function __construct()
     {
-        parent::__construct();      
-        $this->load->model('Login_model'); 
+        parent::__construct();
+        $this->load->model('Login_model');
     }
 
     public function keluar()
     {
-        $this->session->sess_destroy(); 
+        $this->session->sess_destroy();
         redirect('login');
     }
 
     public function index()
-    { 
+    {
         $idjemaat = $this->session->userdata('idjemaat');
         if (!empty($idjemaat)) {
-            redirect( site_url() );
-        }else{
-            $this->load->view('login');     
+            redirect(site_url());
+        } else {
+            $this->load->view('login');
         }
-
     }
 
     public function cek_login()
@@ -31,19 +30,21 @@ class Login extends CI_Controller {
         $username = trim($this->input->post('username'));
         $password = trim($this->input->post('password'));
 
-        if (empty($username) AND empty($password)) {
+        // if (empty($username) AND empty($password)) {
+        if (empty($username) || empty($password)) {
             $pesan = '<div class="alert alert-danger">username atau password tidak boleh kosong!</div>';
             $this->session->set_flashdata('pesan', $pesan);
             redirect('login');
-        }else{
+        } else {
             $kirim = $this->Login_model->cek_login($username, md5($password));
             if ($kirim->num_rows() > 0) {
                 $result = $kirim->row();
 
                 if (empty($result->foto)) {
                     $foto = base_url('images/user-01.png');
-                }else{
-                    $foto = base_url('uploads/pengguna/'.$result.foto);
+                } else {
+                    // $foto = base_url('uploads/pengguna/'.$result.foto);
+                    $foto = base_url('uploads/pengguna/' . $result->foto);
                 }
 
                 $data = array(
@@ -55,17 +56,15 @@ class Login extends CI_Controller {
                     'foto' => $foto,
                 );
 
-                $this->session->set_userdata( $data );  
-                redirect( site_url() );
-            }else{
+                $this->session->set_userdata($data);
+                redirect(site_url());
+            } else {
                 $pesan = '<div class="alert alert-danger">Username atau password salah!</div>';
                 $this->session->set_flashdata('pesan', $pesan);
                 redirect('login');
             }
-
         }
     }
-
 }
 
 /* End of file Login.php */
