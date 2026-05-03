c0f483b832aa07f83bd9a6ec9bb315cbe0ea9b76
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-05-03 21:21:28 +0700

    penambahan api apps mydc+

---
+++ community/application/controllers/api/AppVersion.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AppVersion extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        header('Content-Type: application/json');
+    }
+
+    public function index()
+    {
+        echo json_encode([
+            'status' => true,
+            'data' => [
+                'versi_minimum' => '1.0.14',
+                'versi_terbaru' => '1.0.14',
+                'pesan' => 'Shalom DM. versi baru mydc+ tersedia, silakan update aplikasi untuk mendapatkan fitur terbaru.',
+                'url_playstore' => 'https://play.google.com/store/apps/details?id=com.escIntech.mydcplus',
+            ]
+        ]);
+    }
+}
