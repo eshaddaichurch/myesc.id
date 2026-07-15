f867c9ef7c40d86f03c6b0128ebb60b607cc03fb
eshaddaichurch <126936611+eshaddaichurch@users.noreply.github.com>
2026-07-15 10:10:54 +0700

    sementara mematikan hooks visitor membebani server

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -2,18 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
-| -------------------------------------------------------------------------
-| Hooks
-| -------------------------------------------------------------------------
-| This file lets you define "hooks" to extend CI without hacking the core
-| files.  Please see the user guide for info:
-|
-|	https://codeigniter.com/userguide3/general/hooks.html
-|
-*/
-$hook['post_controller_constructor'][] = array(
-    'class' => 'Track_Visitor',
-    'function' => 'visitor_track',
-    'filename' => 'Track_Visitor.php',
-    'filepath' => 'hooks'
-);
+ * | -------------------------------------------------------------------------
+ * | Hooks
+ * | -------------------------------------------------------------------------
+ * | This file lets you define "hooks" to extend CI without hacking the core
+ * | files.  Please see the user guide for info:
+ * |
+ * |	https://codeigniter.com/userguide3/general/hooks.html
+ * |
+ */
+// $hook['post_controller_constructor'][] = array(
+//     'class' => 'Track_Visitor',
+//     'function' => 'visitor_track',
+//     'filename' => 'Track_Visitor.php',
+//     'filepath' => 'hooks'
+// );
