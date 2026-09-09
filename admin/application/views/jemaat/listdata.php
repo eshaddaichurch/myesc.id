0a85f390b3bf7882dcb6d009dc572939cf6ee3a7
eshaddaichurch <escintech@gmail.com>
2026-09-09 14:00:31 +0700

    perbaikan agar tidak berat saat search

--- admin/application/views/jemaat/listdata.php
+++ admin/application/views/jemaat/listdata.php
@@ -480,6 +480,295 @@ $this->load->view("template/sidemenu");
       "processing": true,
       "serverSide": true,
       "order": [],
+      "searchDelay": 600, // <-- INI YANG DITAMBAHKAN: tunggu 600ms setelah berhenti ngetik sebelum kirim request
+      "ajax": {
+        "url": "<?php echo site_url('jemaat/datatablesource') ?>",
+        "type": "POST"
+      },
+      "columnDefs": [{
+          "targets": [0],
+          "orderable": false,
+          "className": "dt-body-center"
+        },
+        {
+          "targets": [1],
+          "className": "dt-body-center"
+        },
+        {
+          "targets": [2],
+          "className": "dt-body-center"
+        },
+        {
+          "targets": [3],
+          "className": "dt-body-center"
+        },
+        {
+          "targets": [4],
+          "className": "dt-body-center"
+        },
+        {
+          "targets": [5],
+          "className": "dt-body-center"
+        },
+        {
+          "targets": [6],
+          "orderable": false,
+          "className": "dt-body-center",
+          "visible": (idotorisasi == '0000' || idotorisasi == '0001') ? true : false
+        },
+      ],
+
+    });
+
+  }); //end (document).ready
+
+
+  $(document).on("click", "#hapus", function(e) {
+    var link = $(this).attr("href");
+    e.preventDefault();
+
+    bootbox.confirm("Anda yakin ingin menghapus data ini ?", function(result) {
+      if (result) {
+        document.location.href = link;
+      }
+    });
+  });
+
+
+
+
+  $(document).on('click', '#tampilinfojemaat', function(event) {
+    event.preventDefault();
+    $('#modalinfojemaat').modal({
+      backdrop: 'static',
+      keyboard: false
+    });
+    $('#modalinfojemaat').modal('show');
+
+    kosongkanModalInfoJemaat();
+    var idjemaat = $(this).attr("data-idjemaat");
+
+    $.ajax({
+        url: '<?php echo site_url('jemaat/get_info_detail') ?>',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+          'idjemaat': idjemaat
+        },
+      })
+      .done(function(get_info_detailResult) {
+        console.log(get_info_detailResult);
+
+        var jemaat = get_info_detailResult.rsJemaat;
+
+        if (!get_info_detailResult.success) {
+          swal("Informasi", get_info_detailResult.msg, "info");
+          return;
+        }
+
+        $('#tdnoaj').html(jemaat['noaj']);
+
+        $('#tdkewarganegaraan').html(jemaat['kewarganegaraan']);
+
+        $('#tdnamalengkap').html(jemaat['namalengkap']);
+        $('#tdnamapanggilan').html(jemaat['namapanggilan']);
+        $('#tdstatusnikah').html(jemaat['statuspernikahan']);
+        $('#tdjeniskelamin').html(jemaat['jeniskelamin']);
+        $('#tdtempatlahir').html(jemaat['tempatlahir']);
+        $('#tdgoldarah').html(jemaat['golongandarah']);
+        $('#tdnotelp').html(jemaat['notelp']);
+        $('#tdnohp').html(jemaat['nohp']);
+        $('#tdemail').html(jemaat['email']);
+        $('#tdfacebook').html(jemaat['facebook']);
+        $('#tdinstagram').html(jemaat['instagram']);
+        $('#tdalamatrumah').html(jemaat['alamatrumah']);
+        $('#tdrtrw').html(jemaat['rtrw']);
+        $('#tdkelurahan').html(jemaat['kelurahan']);
+        $('#tdkecamatan').html(jemaat['kecamatan']);
+
+        $('#tdkabupatenkota').html(jemaat['kotakabupaten']);
+
+        $('#tdpropinsi').html(jemaat['propinsi']);
+        $('#tdkodepos').html(jemaat['kodepos']);
+        $('#tdstatusjemaat').html(jemaat['statusjemaat']);
+        $('#tdnamadc').html(jemaat['namadc']);
+
+
+        $('#tddarurattelepon').html(jemaat['notelpdarurat']);
+        $('#tddarurathubungan').html(jemaat['hubungan']);
+        $('#tddaruratnama').html(jemaat['namadarurat']);
+
+        $('#tdpendidikanterakhir').html(jemaat['pendidikanterakhir']);
+        $('#tdnamasekolah').html(jemaat['namasekolah']);
+        $('#tdpekerjaan').html(jemaat['pekerjaan']);
+        $('#tdnamaperusahaan').html(jemaat['namaperusahaan']);
+        $('#tdsektorindustri').html(jemaat['sektorindustri']);
+        $('#tdalamatkantor').html(jemaat['alamatkantor']);
+        $('#tdnotelpkantor').html(jemaat['notelpkantor']);
+
+        $('#btnubahkeluarga').attr('data-idjemaat', get_info_detailResult['idencrypt']);
+
+        $('.tdnamajemaat').html(jemaat['namalengkap']);
+        $('.tdtempatlahir').html(jemaat['tempatlahir'] + "/ " + tgldmy(jemaat['tanggallahir']));
+        $('.tdjeniskelamin').html(jemaat['jeniskelamin']);
+        $('.tdnoaj').html(jemaat['noaj']);
+        $('.tdnik').html(jemaat['nik']);
+        $('.tdnohp').html(jemaat['nohp']);
+
+
+        var arrBaptisan = get_info_detailResult.arrBaptisan;
+
+        if (arrBaptisan.length > 0) {
+          $('#tdbaptisantanggalakta').html(arrBaptisan[0]['noakta'] + " / " + arrBaptisan[0]['tglakta']);
+          $('#tdbaptisantanggalbaptis').html(arrBaptisan[0]['tglbaptis']);
+          $('#tdbaptisannamagereja').html(arrBaptisan[0]['namagereja']);
+          $('#tdbaptisannamagembala').html(arrBaptisan[0]['namagembala']);
+
+          if (arrBaptisan[0]['tempatbaptis'] == 'Elshaddai') {
+            $('#trFileAkta').hide();
+          } else {
+            $('#trFileAkta').show();
+            $('#linkFileAktaBaptis').attr("href", arrBaptisan[0]['fileaktalokasi']);
+          }
+        } else {
+          $('#tdbaptisantanggalakta').html('');
+          $('#tdbaptisantanggalbaptis').html('');
+          $('#tdbaptisannamagereja').html('');
+          $('#tdbaptisannamagembala').html('');
+          $('#trFileAkta').hide();
+
+        }
+
+
+        var arrKepala = get_info_detailResult.arrKepalaKeluarga;
+        $('#divKepalaKeluarga').empty();
+        for (var i = 0; i < arrKepala.length; i++) {
+          var addText = `<span><i class="fas fa-chevron-right"></i> ` + arrKepala[i]['namalengkap'] + `</span>`;
+          $('#divKepalaKeluarga').append(addText);
+        }
+
+        var arrIstriAnak = get_info_detailResult.arrIstriAnak;
+        $('#divIstriAnak').empty();
+        for (var i = 0; i < arrIstriAnak.length; i++) {
+          var addText = `<div class="col-12">
+                          <span><i class="fas fa-chevron-right"></i> <span class="badge badge-secondary">` + arrIstriAnak[i]['namahubungan'] + `</span> &nbsp;&nbsp;&nbsp;` + arrIstriAnak[i]['namalengkap'] + `</span>
+                        </div>`;
+          $('#divIstriAnak').append(addText);
+        }
+
+        var arrLainnya = get_info_detailResult.arrLainnya;
+        $('#divLainnya').empty();
+        for (var i = 0; i < arrLainnya.length; i++) {
+          var addText = `<div class="col-12">
+                          <span><i class="fas fa-chevron-right"></i> <span class="badge badge-secondary">` + arrLainnya[i]['namahubungan'] + `</span> &nbsp;&nbsp;&nbsp;` + arrLainnya[i]['namalengkap'] + `</span>
+                        </div>`;
+          $('#divLainnya').append(addText);
+        }
+
+        var rskelas = get_info_detailResult.rskelas;
+        $('#tbodyinfokelas').empty();
+        for (var i = 0; i < rskelas.length; i++) {
+          rskelas[i]
+          if (rskelas[i]['statuslulus'] == '1') {
+            var addText = `<tr>
+                        <td>` + rskelas[i]['namakelas'] + `</td>
+                        <td><i class="fa fa-check text-success"></i></td>
+                        <td>` + rskelas[i]['tglsertifikat'] + `</td>
+                        <td><a href="<?php echo site_url('registrasikelas/cetaksertifikat/') ?>` + rskelas[i]['idregistrasikelas'] + `" class="btn btn-primary btn-sm" target="_blank"> <i class="fa fa-pring"></i> Lihat Sertifikat</a></td>
+                      </tr>
+        `;
+          } else {
+            var addText = `<tr>
+                        <td>` + rskelas[i]['namakelas'] + `</td>
+                        <td><i class="fas fa-times text-danger"></i></td>
+                        <td>-</td>
+                        <td></td>
+                      </tr>
+        `;
+          }
+
+          $('#tbodyinfokelas').append(addText);
+        }
+      })
+      .fail(function() {
+
+
+        console.log("error get get_info_detail");
+      });
+
+  });
+
+  function kosongkanModalInfoJemaat(argument) {
+    $('#tdnoaj').html('');
+    $('#tdkewarganegaraan').html('');
+    $('#tdnamalengkap').html('');
+    $('#tdnamapanggilan').html('');
+    $('#tdstatusnikah').html('');
+    $('#tdjeniskelamin').html('');
+    $('#tdtempatlahir').html('');
+    $('#tdgoldarah').html('');
+    $('#tdnotelp').html('');
+    $('#tdnohp').html('');
+    $('#tdemail').html('');
+    $('#tdfacebook').html('');
+    $('#tdinstagram').html('');
+    $('#tdalamatrumah').html('');
+    $('#tdrtrw').html('');
+    $('#tdkelurahan').html('');
+    $('#tdkecamatan').html('');
+    $('#tdkabupatenkota').html('');
+    $('#tdpropinsi').html('');
+    $('#tdkodepos').html('');
+    $('#tdstatusjemaat').html('');
+    $('#tdnamadc').html('');
+    $('#tddarurattelepon').html('');
+    $('#tddarurathubungan').html('');
+    $('#tddaruratnama').html('');
+    $('#tdpendidikanterakhir').html('');
+    $('#tdnamasekolah').html('');
+    $('#tdpekerjaan').html('');
+    $('#tdnamaperusahaan').html('');
+    $('#tdsektorindustri').html('');
+    $('#tdalamatkantor').html('');
+    $('#tdnotelpkantor').html('');
+    $('#btnubahkeluarga').attr('data-idjemaat', '');
+    $('.tdnamajemaat').html('');
+    $('.tdtempatlahir').html('');
+    $('.tdjeniskelamin').html('');
+    $('.tdnoaj').html('');
+    $('.tdnik').html('');
+    $('.tdnohp').html('');
+    $('#divKepalaKeluarga').empty();
+    $('#divIstriAnak').empty();
+    $('#divLainnya').empty();
+  }
+
+  $(document).on("click", "#btnubahkeluarga", function(e) {
+    var link = $(this).attr("href");
+    var idjemaat = $(this).attr("data-idjemaat");
+    e.preventDefault();
+    if (idjemaat == "") {
+      return;
+    }
+
+    link += idjemaat;
+    document.location.href = link;
+
+  });
+</script>
+
+<!-- 
+<script type="text/javascript">
+  var table;
+
+  $(document).ready(function() {
+
+    //defenisi datatable
+    table = $("#table").DataTable({
+      "select": true,
+      "processing": true,
+      "serverSide": true,
+      "order": [],
       "ajax": {
         "url": "<?php echo site_url('jemaat/datatablesource') ?>",
         "type": "POST"
@@ -758,4 +1047,4 @@ $this->load->view("template/sidemenu");
 
 </body>
 
-</html>
+</html> -->
