11c619436b968830c8721056ae62b7b666a1765c
eshaddaichurch <escintech@gmail.com>
2026-09-11 13:11:01 +0700

    sementara menggunakan woowa kembali sampai whatsapp resmi selesai diverifikasi

--- admin/application/config/whatsapp.php
+++ admin/application/config/whatsapp.php
@@ -2,18 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['whatsapp'] = array(
-    // 'base_url' => 'https://notifapi.com',
-    // 'api_key'  => 'adpa493ec6c-b691-408a-9b92-177e3a001b3f'        
+    'base_url' => 'https://notifapi.com',
+    'api_key'  => 'adpa493ec6c-b691-408a-9b92-177e3a001b3f'        
     
-    'base_url'         => 'https://graph.facebook.com/v21.0',
-    'phone_number_id'  => '1370683659455693',
-    'access_token'     => 'EAAaL9FEph34BSQKn5SJQYHWG9l74qRTXJb1s7eHP5QJ6tprSf0tn4RZCmy33tIgW4BSWjoTZACaTwv2W58EvSz4SQnLDUXRv7xBZATWLbu2O5siIQwRKBujmpe2iXYOhhTF0Be6HKVYttOT7KFDvpeacK7Xyw2E0OYrCcw3APuziSbA0qSdZACarZBC3BhAuH8gZDZD',
-    'verify_token'     => 'Elshaddaichurch270309',
+    // 'base_url'         => 'https://graph.facebook.com/v21.0',
+    // 'phone_number_id'  => '1370683659455693',
+    // 'access_token'     => 'EAAaL9FEph34BSQKn5SJQYHWG9l74qRTXJb1s7eHP5QJ6tprSf0tn4RZCmy33tIgW4BSWjoTZACaTwv2W58EvSz4SQnLDUXRv7xBZATWLbu2O5siIQwRKBujmpe2iXYOhhTF0Be6HKVYttOT7KFDvpeacK7Xyw2E0OYrCcw3APuziSbA0qSdZACarZBC3BhAuH8gZDZD',
+    // 'verify_token'     => 'Elshaddaichurch270309',
 );
 
-$config['whatsapp_hotlines'] = array(
-    'dc'      => array('name' => 'DC Hotline',      'number' => '6285550001133'),
-    'care'    => array('name' => 'Care Hotline',    'number' => '6285550001187'),
-    'connect' => array('name' => 'Connect Hotline', 'number' => '6285550001166'),
-    'Equip'   => array('name' => 'Equip Hotline',   'number' => '6285183023883'),
-);
+// $config['whatsapp_hotlines'] = array(
+//     'dc'      => array('name' => 'DC Hotline',      'number' => '6285550001133'),
+//     'care'    => array('name' => 'Care Hotline',    'number' => '6285550001187'),
+//     'connect' => array('name' => 'Connect Hotline', 'number' => '6285550001166'),
+//     'Equip'   => array('name' => 'Equip Hotline',   'number' => '6285183023883'),
+// );
